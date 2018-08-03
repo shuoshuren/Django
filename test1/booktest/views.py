2d4aa2913c80846550aa1df43b865f44751155e5
shuoshuren <534361124@qq.com>
2018-08-03 23:38:27 +0800

    视图

--- test1/booktest/views.py
+++ test1/booktest/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import *
+
 
 # Create your views here.
+
+def index(request):
+    return  HttpResponse('<b>Hello world</b>')
