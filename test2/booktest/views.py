be76fe5d4cecfea697dc248a3f5fe902304baa91
shuoshuren <534361124@qq.com>
2018-08-08 00:08:42 +0800

    查询

--- test2/booktest/views.py
+++ test2/booktest/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def index(request):
+    # list = BookInfo.books.filter(heroinfo__hcontent__contains='八')
+    list = BookInfo.books.filter(pk__lt = 3)
+    context = {'list':list}
+    return render(request,'booktest/index.html',context)
